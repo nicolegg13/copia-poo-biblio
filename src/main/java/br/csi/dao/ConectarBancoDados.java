08b72d717036ad0528de99c0984d434db0371742
Nicole Grazzioli <85495959+nicolegg13@users.noreply.github.com>
2025-06-13 21:03:31 -0300

    Update ConectarBancoDados.java

--- src/main/java/br/csi/dao/ConectarBancoDados.java
+++ src/main/java/br/csi/dao/ConectarBancoDados.java
@@ -1,4 +1,5 @@
 package br.csi.dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,9 +7,18 @@ import java.sql.SQLException;
 public class ConectarBancoDados {
     public static Connection conectarBancoPostgres() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/poow_trabalho";
+
+        // Pega a URL do banco de dados da variável de ambiente
+        // Se a variável não existir, usa 'localhost' como padrão (para rodar fora do Docker)
+        String dbHost = System.getenv("DB_HOST");
+        if (dbHost == null) {
+            dbHost = "localhost"; // Valor padrão caso não esteja rodando no Docker
+        }
+
+        String url = "jdbc:postgresql://" + dbHost + ":5432/poow_trabalho";
         String user = "postgres";
         String senha = "1234";
+
         return DriverManager.getConnection(url, user, senha);
     }
 }
